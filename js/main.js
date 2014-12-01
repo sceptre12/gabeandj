91986d8cb229c15afc6b4478be8c49a7e601f347
unknown <xthomas@aac07c2.ad.fiu.edu>
2014-12-01 09:56:32 -0500

    removed tester

--- js/main.js
+++ js/main.js
@@ -1,33 +1,11 @@
-$(document).ready(function() {
-	$('.carousel').carousel({
-  interval: 3800
-	});
-});
-
-
-
 $(window).load(function(){
 	$(document.body).fadeIn(4400);
+
 });
 
 
+$(document).ready(function(){
 
-function ClearStorage(){
-	$($('button')[1]).click(function(){
-	localStorage.clear();
-	});
-};
-
-function SaveStorage(){
-	$($('button')[0]).click(function(){
-		var b = 0;
-		for( i = 0; i < $('li').length; i++){
-			b++;
-			if( $('li')[i].innerHTML != ""){
-			   	localStorage.setItem("item"+b, $('li')[i].innerHTML);
-			}else{
-				b--;
-			}
-		}
-	});
-};
+	$('.carousel').carousel({
+  		interval: 3800
+});
