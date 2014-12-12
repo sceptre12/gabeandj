ad0f009945393c3cd9d38ecb6bc0b6f9e188574e
unknown <xthomas@aac07c2.ad.fiu.edu>
2014-12-12 12:01:24 -0500

    Working on about pages

--- js/main.js
+++ js/main.js
@@ -8,4 +8,10 @@ $(document).ready(function(){
 
 	$('.carousel').carousel({
   		interval: 3800
+	});
+	$('.portrait').hover(function(){
+	  $(this).find('img').fadeTo("slow",0.01);
+	}, function(){
+	  $(this).find('img').fadeTo("slow",1);
+	});
 });
