c932b915aba66cf7e1021ade4f33ae3259e9f310
Xavier <xthom001@gmail.com>
2014-12-13 11:12:47 -0500

    Working on proposal page

--- js/main.js
+++ js/main.js
@@ -9,8 +9,9 @@ $(document).ready(function(){
 	$('.carousel').carousel({
   		interval: 3800
 	});
+
+	// when the mouse hovers over the pictures they change to a different color
 	$('.portrait').hover(function(){
-	
 	  $(this).find('img').fadeTo("slow",0.01);
 	}, function(){
 	  $(this).find('img').fadeTo("slow",1);
