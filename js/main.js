d72ae99d9e8de16a05d79f4f93cd1cc488045b4a
Xavier <xthom001@gmail.com>
2014-12-19 11:35:50 -0500

    Switched to a lighter version of animation

--- js/main.js
+++ js/main.js
@@ -1,19 +1,14 @@
 $(document).ready(function(){
-	$('.carousel').carousel({
-  		interval: 3800
-	});
+	
 	// when the mouse hovers over the pictures they change to a different color
 	$('.portrait').hover(function(){
 	  $(this).find('img').fadeTo("slow",0.01);
 	}, function(){
 	  $(this).find('img').fadeTo("slow",1);
-	});
-
-	
-
-
-	
+	});	
 });
 
-
+$('.carousel').carousel({
+  		interval: 3800
+});
 
