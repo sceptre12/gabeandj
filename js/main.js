128460ea871a45a9dfef44e044ec31973d6052df
Xavier <xthom001@gmail.com>
2014-12-21 15:36:02 -0500

    starting to compress these images

--- js/main.js
+++ js/main.js
@@ -6,9 +6,31 @@ $(document).ready(function(){
 	}, function(){
 	  $(this).find('img').fadeTo("slow",1);
 	});	
+	$('.carousel').carousel({
+  		interval: 4800
+	});
 });
 
-$('.carousel').carousel({
-  		interval: 3800
-});
 
+
+// $(window).load(function(){
+// 	var arP =[
+// 		"img/Augustin083.jpg",
+// 		"img/Augustin116.jpg",
+// 		"img/Augustin071.jpg",
+// 		"img/Augustin063.jpg",
+// 		"img/Augustin035.jpg",
+// 		"img/Augustin016.jpg",
+// 		"img/Augustin015.jpg",
+// 		"img/img10mid.jpg",
+// 		"img/img12mid.jpg",
+// 		"img/Augustin022.jpg",
+// 		"img/Augustin067.jpg"
+// 	];
+// 	function preload(arP){
+// 		$(arP).each(function(){
+// 			$('<img />').attr('src',this).appendTo('body').css('display','none');
+// 		});
+// 	}
+	
+// });
