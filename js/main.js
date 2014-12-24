63eb779a803a5d69eeef0b9ce0fb047eda2b2014
Xavier <xthom001@gmail.com>
2014-12-24 01:01:09 -0500

    major changes

--- js/main.js
+++ js/main.js
@@ -13,3 +13,29 @@ $(document).ready(function(){
   		interval: 4800
 	});
 });
+$(window).load(function(){
+	var chk = 'F5$40x9';
+	localStorage.setItem("info" , 'F5$40x9');
+});
+function chek(){
+	if(!($("#guestc").val() === localStorage.getItem("info"))){
+		event.preventDefault();
+		$('#hd').css('opacity','1');
+		$('#hd').fadeTo(600, 0);
+	}
+}
+
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
