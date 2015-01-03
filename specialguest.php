3d34a02eb0bca32058dbc14ec5b3ef48c6a20d5c
Xavier <xthom001@gmail.com>
2015-01-03 13:39:10 -0500

    pushing minified content

--- specialguest.php
+++ specialguest.php
@@ -21,7 +21,7 @@
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="css/bootstrap-theme.min.css">
         <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
-        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/main.min.css">
         <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
     </head>
     <body id="sgsingup" class="container">
@@ -104,6 +104,6 @@
         <script type="text/javascript">
         (function(){var e=sessionStorage.getItem("item");var t=$(".gont");e=e.split("")[0];for(var n=0;n<e;n++){if(n>=1){var r="<h4>Guest "+(n+1)+" </h4>"+'<div class="form-group guest">'+'<label  for="gf'+(n+1)+'">First Name</label>'+'<input type="text" id="gf'+(n+1)+'" name="fgname'+(n+1)+'" placeholder=" First Name">'+'<label for="gl'+(n+1)+'">Last Name</label>'+'<input type="text" id="gl'+(n+1)+'" name="lname'+(n+1)+'" placeholder="Last Name">'+"</div>"+'<div class="form-group">'+'<label for="gft'+(n+1)+'">Meal choice</label>'+'<select name="gfood'+(n+1)+'" id="gft'+(n+1)+'" class="form-control">'+'<option value="Chicken">Chicken</option>'+'<option value="Fish">Fish</option>'+'<option value="Vegetarian">Vegetarian</option>'+"</select>"+'<label for="gallergies'+(n+1)+'">Do They have any Food Allergies</label>'+'<input type="text" name="gallergies'+(n+1)+'" class="form-control" id="allergies'+(n+1)+'" placeholder="Allergies">';t.append(r).find("label").css("margin-right","10px")}}})()
         </script>
-        <script src="js/plugins.js"></script>
+        <script src="js/plugins.min.js"></script>
     </body>
 </html>
