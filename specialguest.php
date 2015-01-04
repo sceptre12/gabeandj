1debdcd16983a0e8f37939727d5d193cdfbeaf71
Xavier <xthom001@gmail.com>
2015-01-04 11:16:05 -0500

    adding in the cdns

--- specialguest.php
+++ specialguest.php
@@ -18,8 +18,7 @@
           else if (localStorage.getItem("used") === sessionStorage.getItem("item")){window.location.replace("accessdenied.html");}
           localStorage.setItem("used",sessionStorage.getItem("item"));
         </script>
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
         <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
         <link rel="stylesheet" href="css/main.min.css">
         <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
@@ -100,7 +99,7 @@
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
-        <script src="js/vendor/bootstrap.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
         <script type="text/javascript">
         (function(){var e=sessionStorage.getItem("item");var t=$(".gont");e=e.split("")[0];for(var n=0;n<e;n++){if(n>=1){var r="<h4>Guest "+(n+1)+" </h4>"+'<div class="form-group guest">'+'<label  for="gf'+(n+1)+'">First Name</label>'+'<input type="text" id="gf'+(n+1)+'" name="fgname'+(n+1)+'" placeholder=" First Name">'+'<label for="gl'+(n+1)+'">Last Name</label>'+'<input type="text" id="gl'+(n+1)+'" name="lname'+(n+1)+'" placeholder="Last Name">'+"</div>"+'<div class="form-group">'+'<label for="gft'+(n+1)+'">Meal choice</label>'+'<select name="gfood'+(n+1)+'" id="gft'+(n+1)+'" class="form-control">'+'<option value="Chicken">Chicken</option>'+'<option value="Fish">Fish</option>'+'<option value="Vegetarian">Vegetarian</option>'+"</select>"+'<label for="gallergies'+(n+1)+'">Do They have any Food Allergies</label>'+'<input type="text" name="gallergies'+(n+1)+'" class="form-control" id="allergies'+(n+1)+'" placeholder="Allergies">';t.append(r).find("label").css("margin-right","10px")}}})()
         </script>
